af396f8aa83d84f60034ce349ca803b46c79c2e1
AlexeiZhernosek1991 <115401266+AlexeiZhernosek1991@users.noreply.github.com>
2023-05-28 23:26:17 +0300

    add_sclad 28.05.2023

---
+++ Main.py
@@ -0,0 +1,110 @@
+from functools import partial
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import add_sclad
+
+
+# python -m PyQt5.uic.pyuic -x [FILENAME].ui -o [FILENAME].py
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 40, 161, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(210, 40, 201, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(450, 40, 171, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(650, 40, 131, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(400, 120, 271, 71))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(590, 440, 171, 71))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        font.setStrikeOut(False)
+        self.pushButton_6.setFont(font)
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(270, 250, 261, 81))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setObjectName("label")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(510, 90, 41, 31))
+        self.line.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить товар"))
+        self.pushButton_2.setText(_translate("MainWindow", "Оформить покупку"))
+        self.pushButton_3.setText(_translate("MainWindow", "Перемещение"))
+        self.pushButton_4.setText(_translate("MainWindow", "Списать"))
+        self.pushButton_5.setText(_translate("MainWindow", "Подтвердить перемещение"))
+        self.pushButton_6.setText(_translate("MainWindow", "Добавить склад"))
+        self.label.setText(_translate("MainWindow", "Добро пожаловать"))
+        self.pushButton_6.clicked.connect(self.add_sklad)
+
+    def add_sklad(self):
+        Dialog = QtWidgets.QDialog()
+        ui2 = add_sclad.Ui_Dialog()
+        ui2.setupUi(Dialog)
+        ui2.gen_tabl()
+        Dialog.show()
+        Dialog.exec_()
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ add_sclad.py
@@ -0,0 +1,142 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
+
+config = [[1, 'sklad1', 'adres1'], [2, 'sklad2', 'adres2'], [3, 'sklad3', 'adres3'], [4, 'sklad1', 'adres4'],
+          [5, 'sklad2', 'adres5'], [6, 'sklad3', 'adres1']]
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(569, 381)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(110, 30, 221, 31))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(20, 110, 181, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.textEdit.setFont(font)
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit2.setGeometry(QtCore.QRect(20, 180, 181, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.textEdit2.setFont(font)
+        self.textEdit2.setObjectName("textEdit2")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 80, 141, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(20, 150, 141, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(20, 320, 93, 28))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 220, 93, 28))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(20, 250, 120, 28))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.tableWidget = QtWidgets.QTableWidget(Dialog)
+        self.tableWidget.setGeometry(QtCore.QRect(220, 110, 331, 201))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(300, 80, 191, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Добавление склада"))
+        self.label_2.setText(_translate("Dialog", "Название склада"))
+        self.label_4.setText(_translate("Dialog", "Адрес склада"))
+        self.pushButton.setText(_translate("Dialog", "Отменить"))
+        self.pushButton_2.setText(_translate("Dialog", "Добавить"))
+        self.pushButton_3.setText(_translate("Dialog", "Удалить склад"))
+        self.label_3.setText(_translate("Dialog", "Существующие склады"))
+        self.pushButton_2.clicked.connect(self.add_sclad)
+        self.pushButton_3.clicked.connect(self.del_sclad)
+        self.pushButton.clicked.connect(self.clear_text)
+
+    # генерирует таблицу
+    def gen_tabl(self):
+        try:
+            self.table_name = 'Существующие склады'
+            self.tableWidget.setColumnCount(len(config[0]))
+            self.tableWidget.setRowCount(len(config))
+            self.tableWidget.setHorizontalHeaderLabels(['id', 'name'])
+            row = 0
+            for tup in config:
+                col = 0
+                for item in tup:
+                    cell_info = QTableWidgetItem(str(item))
+                    self.tableWidget.setItem(row, col, cell_info)
+                    col += 1
+                row += 1
+        except:
+            pass
+
+    # Функция считывает введенное значение, затем записывает его в переменную обновляет таблицу,
+    # стирает введенное значение
+    def add_sclad(self):
+        get_val = getattr(self, 'textEdit')
+        get_val2 = getattr(self, 'textEdit2')
+        config.append([12134, get_val.toPlainText(), get_val2.toPlainText()])
+        self.gen_tabl()
+        self.textEdit.clear()
+        self.textEdit2.clear()
+
+    def del_sclad(self):
+        row_ = self.tableWidget.currentRow()
+        item_ = self.tableWidget.item(row_, 0)
+        item_ = item_.text()
+        print(item_)
+        for i in config:
+            if int(item_) in i:
+                a = config.index(i)
+                config.pop(a)
+        print(config)
+        self.gen_tabl()
+
+    def clear_text(self):
+        self.textEdit.clear()
+        self.textEdit2.clear()
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    ui.gen_tabl()
+    Dialog.show()
+    sys.exit(app.exec_())
